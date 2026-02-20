3db788aa7a9b95a0a93e579d6b8fe2d71c230c8f
Travis Covert <tcovert2015@gmail.com>
2026-02-20 12:55:56 -0600

    Updates

--- examples/radiant-counter/.coverteda_build.tcl
+++ examples/radiant-counter/.coverteda_build.tcl
@@ -3,8 +3,6 @@
 # Top: counter
 
 prj_open "//wsl.localhost/Ubuntu-24.04/home/tcovert/projects/CovertEDA/examples/radiant-counter/counter.rdf"
-prj_set_strategy_value -strategy Strategy1 {SYN_Tool=LSE}
-prj_save
 prj_run_synthesis
 prj_run_map
 prj_run_par
